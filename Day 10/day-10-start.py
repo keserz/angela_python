6cd3a1e4cc300f7df675b36b3205194ac76c561d
keserz <Fouad.Gasmi@etu.univ-grenoble-alpes.fr>
2022-07-29 00:03:07 +0200

    end of first course day 10

--- Day 10/day-10-start.py
+++ Day 10/day-10-start.py
@@ -19,12 +19,12 @@
 ### la version simplifiée ###
 
 def format_name(first_name, last_name):
+	"""Take a first and last name and format it to return the title case version of the name"""
 	if first_name == "" or last_name == "":
 		return "You didn't provide valid inputs"
 	f_name = first_name.title()
 	l_name = last_name.title()
 
 	return f"{f_name} {l_name}"
-
 
 print(format_name("", ""))
